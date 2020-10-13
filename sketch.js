7aac4d0783d3ed8b8c1af9b14023e40cfc4324da
Joseph Fischetti <fischettijw@gmail.com>
2020-10-13 10:18:54 -0400

    added NOLOOP and drawGrahpic and reformat numeric output

--- sketch.js
+++ sketch.js
@@ -1,61 +1,87 @@
-const diam = 800;
-let dartWeight = 1;
+const diam = 1000;
+let digitsOfPie;
+let dartWeight = 10;
 let darts = 0;
+const dartMinimum = 200000;
 let inCircle = 0;
 let ratio = 0;
-let pie;
+let pie = 0;
 let pieDiv;
-let batchDarts = 10000;
+const lowStop = 3.1415;
+const highStop = 3.1417;
+
+const batchDarts = 1000;
+const drawGraphic = true;
+
+const pieRandomDigits = 3;
+// let randomMethod = rndPie; // diam must be 1000
+let randomMethod = p5js;
 
 function setup() {
     // frameRate(1);
-    createCanvas(diam, diam);
-    background(220);
-    strokeWeight(1);
-    stroke('red');
-    fill('white');
-    circle(diam / 2, diam / 2, diam);
+    if (drawGraphic) {
+        createCanvas(diam, diam);
+        background(220);
+        strokeWeight(1);
+        stroke('red');
+        fill('white');
+        circle(diam / 2, diam / 2, diam);
+    }
 
-    // createP("hello").style('font-size', '72pt');
-    pieDiv = createDiv().style('font-size', '18pt');
-    // pieDiv = createDiv();
-    // pieDiv = createDiv(pie);
-    // pieDiv.style('font-size', '18pt');
-
+    pieDiv = createDiv().style('font-size', '24pt');
+    digitsOfPie = Array.from(getPie()); // getPie() in pieDigits.js
 }
 
 function draw() {
+
     generateDarts(batchDarts);
-    stroke(255, 0, 0);
-    fill(255, 0, 0, 0);
-    circle(diam / 2, diam / 2, diam);
-    stroke('red');
-    pieDiv.html(`${diam} <br> 
-                 ${batchDarts} <br>
-                 ${nf(pie,1,5)} <br> 
-                 ${darts} <br>
-                 ${nf((PI - pie) * 100 / PI,1,4)} %`);
+    if (drawGraphic) {
+        stroke(255, 0, 0);
+        fill(255, 0, 0, 0);
+        circle(diam / 2, diam / 2, diam);
+        stroke('red');
+    }
+    pieDiv.html(`Diameter &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp ${diam} <br> 
+                 Batch Darts &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp ${batchDarts} <br>
+                 Simulated Pie ;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp ${nf(pie,1,5)} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp ${lowStop}0 - ${highStop}0 <br> 
+                 Number of Darts ;&nbsp;&nbsp;&nbsp${darts} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp \> ${dartMinimum} <br>
+                 % Deviation ;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp${nf((PI - pie) * 100 / PI,1,5)} %`);
     // JS Template literals   ` ackticks above tilde symbol
+
+    if (pie > lowStop && pie < highStop && darts > dartMinimum) {
+        noLoop();
+    }
+
 }
 
 function generateDarts(n) {
     let r = diam / 2;
-    strokeWeight(dartWeight);
+    if (drawGraphic) { strokeWeight(dartWeight); }
     for (let i = 0; i < n; i++) {
-        let x = random(0, diam);
-        let y = random(0, diam);
-        let dartLenght = sqrt((x - r) * (x - r) + (y - r) * (y - r));
+        let x = randomMethod();
+        let y = randomMethod();
+        let dartLenght = dist(x, y, r, r);
+        // let dartLenght = sqrt((x - r) * (x - r) + (y - r) * (y - r));
         if (dartLenght < r) {
-            stroke('red');
+            if (drawGraphic) { stroke('red'); }
             inCircle++;
         } else {
-            stroke('black');
+            if (drawGraphic) { stroke('black'); }
         }
-        point(x, y);
+        if (drawGraphic) { point(x, y) };
     }
     ratio = inCircle / (frameCount * n);
     darts += n;
     pie = 4 * ratio;
+
+}
+
+function p5js() {
+    return random(0, diam);
+}
+
+function rndPie() {
+    return randomPie(pieRandomDigits);
 }
 
 
